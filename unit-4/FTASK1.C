9d604233ec2c63828d4035516688bfc43148c707
zalavadiyafenny <125332245+zalavadiyafenny@users.noreply.github.com>
2023-02-16 09:58:00 +0530

    Add files via upload

---
+++ unit-4/FTASK1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int x,y;
+    clrscr();
+    printf("enter the value of x :");
+    scanf("%d",&x);
+    printf("enter the value of y:");
+    scanf("%d",&y);
+    printf("the sum of x and y: %d",x+y*2);
+    getch();
+
+}
---
+++ unit-4/FTASK5.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int x,y,z;
+    clrscr();
+    printf("enter the value of x :");
+    scanf("%d",&x);
+    printf("enter the value of y:");
+    scanf("%d",&y);
+     printf("enter the value of z:");
+    scanf("%d",&z);
+    printf("the sum of x and y: %d",x+y+z*3);
+    getch();
+
+}
